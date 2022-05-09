157c4981b797df3270b78e1b48e62fb8ea319d36
VortexOnManjaro <59007159+Yakkhini@users.noreply.github.com>
2022-05-09 23:06:11 +0800

    Add: Board with texture.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use bevy::{prelude::*, window::PresentMode, sprite::Anchor};
+
+#[derive(Component)]
+struct Board;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "I am a window!".to_string(),
+            width: 1080.,
+            height: 720.,
+            present_mode: PresentMode::Fifo,
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(hello_world_system)
+        .add_startup_system(set_camera)
+        .add_startup_system(spawn_board)
+        .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
+        .run();
+}
+
+fn hello_world_system() {
+    println!("hello world");
+}
+
+fn set_camera(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn spawn_board(mut commands: Commands, assets_server: Res<AssetServer>) { 
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(200.0,200.0)),
+            anchor: Anchor::TopCenter,
+            ..default()
+        },
+        texture: assets_server.load("sprites/Sprite-0001.png"),
+        ..default()
+    })
+    .insert(Board);
 }
