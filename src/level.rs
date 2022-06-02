8a9b5881867e7334f2ae443cab2b177c5359992a
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2022-06-02 15:20:08 +0800

    Feat: Level choose system UI.

--- src/level.rs
+++ src/level.rs
@@ -14,15 +14,87 @@ use bevy::prelude::*;
 
 use crate::appstate;
 
-fn level_confirm(mut state: ResMut<State<appstate::AppState>>) {
-    state.set(appstate::AppState::InGame).unwrap();
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+#[derive(Component)]
+struct LevelButton;
+
+fn setup_level_button(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                // center button
+                margin: Rect::all(Val::Auto),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            color: NORMAL_BUTTON.into(),
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle {
+                text: Text::with_section(
+                    "level 1",
+                    TextStyle {
+                        font: asset_server.load("fonts/mplus_hzk_12.ttf"),
+                        font_size: 40.0,
+                        color: Color::rgb(0.9, 0.9, 0.9),
+                    },
+                    Default::default(),
+                ),
+                ..default()
+            });
+        })
+        .insert(LevelButton)
+        .insert(Name::new("Level Button"));
+}
+
+fn level_button_system(
+    mut state: ResMut<State<appstate::AppState>>,
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color) in interaction_query.iter_mut() {
+        match *interaction {
+            Interaction::Clicked => {
+                *color = PRESSED_BUTTON.into();
+                state.set(appstate::AppState::InGame).unwrap();
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+fn level_clean_up(mut commands: Commands, mut query: Query<Entity, With<LevelButton>>) {
+    for item in query.iter_mut() {
+        commands.entity(item).despawn()
+    }
 }
 
 pub struct LevelPlugin;
 impl Plugin for LevelPlugin {
     fn build(&self, app: &mut App) {
         app.add_system_set(
-            SystemSet::on_update(appstate::AppState::Level).with_system(level_confirm),
+            SystemSet::on_enter(appstate::AppState::Level).with_system(setup_level_button),
+        );
+        app.add_system_set(
+            SystemSet::on_update(appstate::AppState::Level).with_system(level_button_system),
+        );
+        app.add_system_set(
+            SystemSet::on_exit(appstate::AppState::Level).with_system(level_clean_up),
         );
     }
 }
